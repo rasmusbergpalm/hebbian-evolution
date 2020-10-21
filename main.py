c53d3f0ec403205b7ea1043c8f0842eecd4d3df3
rasmusbergpalm <rasmusbergpalm@gmail.com>
2020-10-21 14:45:24 +0200

    GMM, sigma=0.1, Adam lr=0.1, rank fitness shaping, M=8

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     num_learning_rules = 8
     population = GMMHebbianPopulation(num_learning_rules, scale)
 
+
     learning_rate = 0.1
     iterations = 300
     pop_size = 200
