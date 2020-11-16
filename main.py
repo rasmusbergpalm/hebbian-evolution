7af9ef33a3653a467f9971044987300509fd130a
rasmusbergpalm <rasmusbergpalm@gmail.com>
2020-11-16 10:29:25 +0100

    Car, Sticky, Static, 50 pop

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import tqdm
 from evostrat import NormalPopulation, compute_centered_ranks
 from torch.multiprocessing import Pool, set_start_method
 from torch.optim import Adam
-import util
-from hebbian_agent import HebbianCarRacingAgent
-from meta_agent import MetaAgent
-from static_car import StaticCarRacingAgent
 
 # noinspection PyUnresolvedReferences
 import envs
+import util
+from meta_agent import MetaAgent
+from static_car import StaticCarRacingAgent
 
 if __name__ == '__main__':
     set_start_method('spawn')
@@ -18,20 +17,19 @@ if __name__ == '__main__':
     train_writer, test_writer = util.get_writers('hebbian')
 
     env_args = [
-        # {},
+        {},
         {'side_force': 10.0},
         {'side_force': -10.0},
         {'friction': 0.5},
-        {'friction': 2.0}
+        # {'friction': 2.0}
     ]
 
 
-
     def constructor(params) -> MetaAgent:
-        return MetaAgent([HebbianCarRacingAgent.from_params(params, env_arg) for env_arg in env_args])
+        return MetaAgent([StaticCarRacingAgent.from_params(params, env_arg) for env_arg in env_args])
 
 
-    agent = HebbianCarRacingAgent(env_args[0])
+    agent = StaticCarRacingAgent({})
     shapes = {k: p.shape for k, p in agent.get_params().items()}
     population = NormalPopulation(shapes, constructor, std=0.1)
 
