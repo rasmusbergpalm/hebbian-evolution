5031e6dd7bd683d26c28215fa944b5ac6c7856cf
rasmusbergpalm <rasmusbergpalm@gmail.com>
2021-04-07 11:11:31 +0200

    adds static ant

---
+++ agents/ant/static_ant.py
@@ -0,0 +1,32 @@
+from typing import Dict
+
+import torch as t
+import torch.nn.functional as f
+
+from agents.ant.ant import Ant
+
+
+class StaticAnt(Ant):
+    def __init__(self, params: Dict[str, t.Tensor], env_args: Dict):
+        super().__init__(env_args)
+        self.params = params
+
+    def policy(self, x):
+        x = t.tanh(f.linear(x, self.params["linear.1"].t()))
+        x = t.tanh(f.linear(x, self.params["linear.2"].t()))
+        x = t.tanh(f.linear(x, self.params["linear.3"].t()))
+        return x
+
+    @staticmethod
+    def param_shapes() -> Dict[str, t.Tensor]:
+        return {
+            'linear.1': (28, 128),
+            'linear.2': (128, 64),
+            'linear.3': (64, 8),
+        }
+
+
+if __name__ == '__main__':
+    params = {k: 0.1 * t.randn(s) for k, s in StaticAnt.param_shapes().items()}
+    ant = StaticAnt(params, {})
+    print(ant.fitness(True))
